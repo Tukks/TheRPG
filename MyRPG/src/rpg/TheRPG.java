f18e99d421645f2ba6e936b94a9622c29256b910
Tumeconnais <Giuseppe06@gmail.com>
2013-11-15 23:37:37 +0100

    Fonctionnement Chargement dynamique

--- MyRPG/src/rpg/TheRPG.java
+++ MyRPG/src/rpg/TheRPG.java
@@ -7,9 +7,6 @@
 package rpg;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -28,27 +25,51 @@ public class TheRPG extends SecureClassLoader {
 	public ArrayList<File> path = new ArrayList<File>();  
     /**
      * @param args the command line arguments
-     * @throws MalformedURLException 
-     * @throws InvocationTargetException 
-     * @throws IllegalArgumentException 
-     * @throws SecurityException 
-     * @throws NoSuchMethodException 
-     * @throws ClassNotFoundException 
-     * @throws IllegalAccessException 
-     * @throws InstantiationException 
-     * @throws URISyntaxException 
+     * 
      */
-    public static void main(String[] args) throws MalformedURLException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException, URISyntaxException {
+    public static void main(String[] args) {
         System.out.println("Hello");
         TheRPG test = new TheRPG();
-        test.ChargementDyn();
+        try {
+			test.ChargementDyn();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         //Un commentaire
     }
-    void ChargementDyn() throws MalformedURLException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+    void ChargementDyn() throws MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
     	URLClassLoader cl = new URLClassLoader(new URL[] { new File("C:\\MyPlug.jar").toURI().toURL() });
-    	URL[] url = new URL[] { new File("C:\\MyPlug.jar").toURI().toURL() };
-    	System.out.println(url[0].toString());
-    	java.lang.Class.forName("MyPlugin", false, cl);
+    	Class<?> cl1 =  cl.loadClass("plugin.MyPlugin");
+    	Object o =  cl1.newInstance(); 
+    	Method m = cl1.getMethod("run");
+    	m.invoke(o);
+    	
+    	//Method m = cl1.getDeclaredMethod("run");
+    	
+
+    	
     }
    
 }
