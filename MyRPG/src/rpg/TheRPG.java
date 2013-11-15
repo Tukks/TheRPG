8756e8c46a9aa8b224ddbed2a6305eb6a8259930
Tumeconnais <Giuseppe06@gmail.com>
2013-11-15 16:21:42 +0100

    Chargement dynamique indicatoion : DL le MyPlug.jar sur fb

--- MyRPG/src/rpg/TheRPG.java
+++ MyRPG/src/rpg/TheRPG.java
@@ -6,18 +6,49 @@
 
 package rpg;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.SecureClassLoader;
+import java.util.ArrayList;
+
+
 /**
  *
  * @author giuse_000
  */
-public class TheRPG {
-
+public class TheRPG extends SecureClassLoader {
+	public ArrayList<File> path = new ArrayList<File>();  
     /**
      * @param args the command line arguments
+     * @throws MalformedURLException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws ClassNotFoundException 
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     * @throws URISyntaxException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException, URISyntaxException {
         System.out.println("Hello");
+        TheRPG test = new TheRPG();
+        test.ChargementDyn();
         //Un commentaire
     }
-    
+    void ChargementDyn() throws MalformedURLException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+    	URLClassLoader cl = new URLClassLoader(new URL[] { new File("C:\\MyPlug.jar").toURI().toURL() });
+    	URL[] url = new URL[] { new File("C:\\MyPlug.jar").toURI().toURL() };
+    	System.out.println(url[0].toString());
+    	java.lang.Class.forName("MyPlugin", false, cl);
+    }
+   
 }
