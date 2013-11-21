c82884c88941bd58ad4f3c6b4b5ee47ea1853d63
Bastien <Bastien@10.154.117.229>
2013-11-21 13:37:35 +0100

    maj interface

--- MyRPG/src/gui/InterfaceRPG.java
+++ MyRPG/src/gui/InterfaceRPG.java
@@ -1,44 +1,48 @@
 package gui;
-import org.eclipse.swt.widgets.*;
-import org.eclipse.swt.events.KeyAdapter;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.*;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import util.PathManager;
 
 public class InterfaceRPG {
-	
+
 	private static Display display;
 
-  public static void centrerSurEcran(Display display, Shell shell) {
-    Rectangle rect = display.getClientArea();
-    Point size = shell.getSize();
-    int x = (rect.width - size.x) / 2;
-    int y = (rect.height - size.y) / 2;
-    shell.setLocation(new Point(x, y));
-  }
-  
-  public static void main(String[] args) {
-    display = new Display();
-    Shell shell = new Shell(display, SWT.CLOSE | SWT.MIN); 
-    shell.setSize(1024, 700);
-    ImageData cursor_Image = new ImageData("C:\\Users\\Bastien\\workspace\\InterfaceRPG\\src\\cursor.png"); 
+	public static void centrerSurEcran(Display display, Shell shell) {
+		Rectangle rect = display.getClientArea();
+		Point size = shell.getSize();
+		int x = (rect.width - size.x) / 2;
+		int y = (rect.height - size.y) / 2;
+		shell.setLocation(new Point(x, y));
+	}
 
-    Cursor cursor1 = new Cursor(display, cursor_Image, 1, 1);
-shell.setCursor(cursor1);    
+	public static void main(String[] args) {
+		display = new Display();
+		Shell shell = new Shell(display, SWT.CLOSE | SWT.MIN);
+		shell.setSize(1024, 700);
+		ImageData cursor_Image = new ImageData(PathManager.cursorImg);
 
-    
-    Image bg_Image = new Image(display, "C:\\Users\\Bastien\\workspace\\InterfaceRPG\\src\\bg.jpg"); 
-    shell.setBackgroundImage(bg_Image);
+		Cursor cursor1 = new Cursor(display, cursor_Image, 1, 1);
+		shell.setCursor(cursor1);
 
-    centrerSurEcran(display, shell);
+		Image bg_Image = new Image(display, PathManager.bgImg);
+		shell.setBackgroundImage(bg_Image);
 
-    shell.open();
+		centrerSurEcran(display, shell);
 
-    while (!shell.isDisposed())
-      if (!display.readAndDispatch())
-        display.sleep();
+		shell.open();
 
-    display.dispose();
-  }
+		while (!shell.isDisposed())
+			if (!display.readAndDispatch())
+				display.sleep();
+
+		display.dispose();
+	}
 }
---
+++ MyRPG/src/util/PathManager.java
@@ -0,0 +1,9 @@
+package util;
+
+public class PathManager {
+
+	public static String bgImg = "img/bg.jpg";
+	public static String cursorImg = "img/cursor.png";
+
+	
+}
