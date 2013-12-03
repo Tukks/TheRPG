21ed934e097d2bf4760476084cac1654c8175b9b
Bastien <Bastien@10.154.117.229>
2013-12-03 09:48:55 +0100

    interface tte pourrie

--- MyRPG/src/util/PathManager.java
+++ MyRPG/src/util/PathManager.java
@@ -2,8 +2,10 @@ package util;
 
 public class PathManager {
 
-	public static String bgImg = "img/bg.jpg";
-	public static String cursorImg = "img/cursor.png";
+	public static final String classImg = "img/classe.jpg";
+	public static final String itemImg = "img/arme.png";
+	public static final String bgGroup = "img/bgGroup.jpg";
+	public static final String bgImg = "img/bg.jpg";
+	public static final String cursorImg = "img/cursor.png";
 
-	
 }
